a93ff3b22f2e394bf27dff81f840c1340b33ecd5
Brian Schneider <57152129+Bschneider22@users.noreply.github.com>
2023-05-03 02:28:24 -0400

    Finished the Pathfinders
    Finished Pathfinders

--- js/index.js
+++ js/index.js
@@ -2,6 +2,18 @@ const delay = (time) => {
   return new Promise(resolve => setTimeout(resolve, time));
 }
 
+const visitedAnimation = (t, color) => {
+  t.element.style.opacity = 0;
+  t.element.style.backgroundColor = color;
+  t.element.offsetWidth;
+  t.element.style.opacity = 1;
+  t.element.style.transition = 'opacity 0.4s ease-in-out';
+
+  t.element.addEventListener('transitionend', function() {
+    t.element.style.transition = '';
+  });
+}
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // TILES CLASS
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -21,6 +33,14 @@ class Tile {
     this.row = r;
     this.col = c;
     this.neighbors = [];
+  }
+
+  getTileRow = () => {
+    return this.row;
+  }
+
+  getTileCol = () => {
+    return this.col;
   }
 
   isTileWall = () => {
@@ -64,8 +84,8 @@ class Tile {
         endTile = this.number;
         previousEndTile = this;
       }
-  
-      this.element.style.backgroundColor = selectedColor;
+      
+      visitedAnimation(this, selectedColor);
     }
 
     if (selectedColor === 'black') {
@@ -108,7 +128,7 @@ class Tile {
     tile.addEventListener('mousemove', () => {
       if (isMouseDown && editMode && selectedColor === 'black' && !this.isTileWall()) {
         this.setTileWall();
-        tile.style.backgroundColor = selectedColor;
+        visitedAnimation(this, selectedColor);
 
         if (this.number === endTile) {
           endTile = -1;
@@ -119,7 +139,7 @@ class Tile {
 
       if (isMouseDown && pathFindingDone) {
         if (clickedOnToMove == 0) {
-          if (!this.isTileWall() && this.number !== startTile) {
+          if (!this.isTileWall() && this.number !== startTile && previousEndTile !== this) {
             if (previousEndTile) {
               previousEndTile.element.style.backgroundColor = 'white';
             }
@@ -127,13 +147,13 @@ class Tile {
             this.setTileNotWall();
             endTile = this.number;
             previousEndTile = this;
-            this.element.style.backgroundColor = "red";
+            this.element.style.backgroundColor = 'red';
           }
           
         }
 
         if (clickedOnToMove == 1) {
-          if (!this.isTileWall() && this.number !== endTile) {
+          if (!this.isTileWall() && this.number !== endTile && previousStartTile !== this) {
             if (previousStartTile) {
               previousStartTile.element.style.backgroundColor = 'white';
             }
@@ -141,7 +161,7 @@ class Tile {
             this.setTileNotWall();
             startTile = this.number;
             previousStartTile = this;
-            this.element.style.backgroundColor = "green";
+            this.element.style.backgroundColor = 'green';
           }
           
         }
@@ -151,19 +171,24 @@ class Tile {
         } else if (currentAlg == 1) {
           dfsTime(0);
         } else if (currentAlg == 2) {
-          dijTime(0);
+          dijTime(0, 'euclidean');
         } else if (currentAlg == 3) {
-          astarTime(0);
+          dijTime(0, 'manhattan');
         } else if (currentAlg == 4) {
-          greedyTime(0);
+          astarTime(0, 'euclidean');
+        } else if (currentAlg == 5) {
+          astarTime(0, 'manhattan');
+        } else if (currentAlg == 6) {
+          greedyTime(0, 'euclidean');
+        } else if (currentAlg == 7) {
+          greedyTime(0, 'manhattan');
         }
       }
 
       
 
-      
 
-      
+
     });
     
     document.addEventListener('mouseup', () => {
@@ -193,8 +218,8 @@ function maze() {
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Grid Size
-const gridSizeX = 20;
-const gridSizeY = 10;
+const gridSizeX = 37;
+const gridSizeY = 15;
 let tileNumber = gridSizeX * gridSizeY;
 let tiles = [];
 let editMode = true;
@@ -205,12 +230,12 @@ const createTiles = () => {
   tiles = [];
   const gridContainer = document.querySelector(".grid-container");
   gridContainer.innerHTML = "";
-  const tileSize = 74;
+  const tileSize = 49;
 
   // Appends the tiles to the grid Container
   for (let i = 0; i < tileNumber; i++) {
     row = Math.floor(i / gridSizeX);
-    col = i % gridSizeY;
+    col = i % gridSizeX;
     const tile = new Tile(i, row, col);
     const tileElement = tile.createElement(tileSize);
     tiles.push(tile);
@@ -233,7 +258,7 @@ const updateNeighbors = (tile, allTiles) => {
   // Add the neighboring tiles to the neighbors array
   indices.forEach((index) => {
     // Check if the index is within the bounds of the array
-    if (index >= 0 && index < allTiles.length) {
+    if (index >= 0 && index < tileNumber) {
       const neighbor = allTiles[index];
 
       // Check if the neighbor is in the same row or column as the tile
@@ -310,23 +335,14 @@ const buildGraph = () => {
 
 const resetVisitedTiles = () => {
   for (let i = 0; i < visitedTiles.length; i++) {
-    visitedTiles[i].element.style.backgroundColor = 'white';
+    if (visitedTiles[i].number !== startTile && visitedTiles[i].number !== endTile) {
+      visitedTiles[i].element.style.backgroundColor = 'white';
+    }
   }
 
   visitedTiles = [];
 }
 
-const visitedAnimation = (t, color) => {
-  t.element.style.opacity = 0;
-  t.element.style.backgroundColor = color;
-  t.element.offsetWidth;
-  t.element.style.opacity = 1;
-  t.element.style.transition = 'opacity 0.4s ease-in-out';
-
-  t.element.addEventListener('transitionend', function() {
-    t.element.style.transition = '';
-  });
-}
 
 const checkToStart = () => {
   return (endTile > -1 && startTile > -1);
@@ -512,17 +528,43 @@ async function dfsTime(delayTime) {
 // Dijkstra
 ///////////////////////
 
-const dijkstra = () => {
+const addToQueueDij = (queue, tile, type) => {
+  let startR = getRow(startTile);
+  let startC = getCol(startTile);
+  let dis = -1;
+
+  if (type === 'euclidean') {
+    dis = Math.sqrt(Math.pow(getCol(tile) - startC, 2) + Math.pow(getRow(tile) - startR, 2));
+  } else if (type === 'manhattan') {
+    dis = Math.abs(getCol(tile) - startC) + Math.abs(getRow(tile) - startR);
+  }
+
+  const newItem = [tile, dis];
+
+  let i = 0;
+
+  // Iterate through the queue and find the index where the new item should be inserted based on its fCost
+  while (i < queue.length && queue[i][1] <= dis) {
+    i++;
+  }
+
+  // Insert the new item at the correct index
+  queue.splice(i, 0, newItem);
+
+  return queue;
+}
+
+const dijkstra = (type) => {
   pathFindingDone = false;
-  currentAlg = 2;
-  dijTime(30)
+  if (type === 'euclidean') {
+    currentAlg = 2;
+  } else {
+    currentAlg = 3;
+  }
+  dijTime(30, type)
 }
 
-const calculateFCost = (tile) => {
-  
-}
-
-async function dijTime(delayTime) {
+async function dijTime(delayTime, type) {
   selectedColor = '';
   if (!checkToStart()) {
     return;
@@ -533,12 +575,12 @@ async function dijTime(delayTime) {
   updateTileNeighbors();
   const graph = buildGraph();
   const visited = new Set([startTile]);
-  const queue = [[startTile, 0]];
+  let prioQueue = [[startTile, 0]];
   const prev = {};
   let endNode = null;
   resetVisitedTiles();
 
-  outerLoop : while (queue.length > 0) {
+  outerLoop : while (prioQueue.length > 0) {
     if (resetOn) {
       break;
     }
@@ -546,16 +588,49 @@ async function dijTime(delayTime) {
       await delay(delayTime);
     } 
 
+    const [node, distance] = prioQueue.shift();
+
     if (node == endTile) {
       endNode = node;
       break;
     }
-
-    /////////
-    // Steps
-    // 1. 
+    
+    for (let neighbor of graph[node]) {
+      if (!visited.has(neighbor)) {
+        visited.add(neighbor);
+        prioQueue = addToQueueDij(prioQueue, neighbor, type);
+        
+        if (neighbor !== startTile && neighbor !== endTile) {
+          if (delayTime == 0) {
+            tiles[neighbor].element.style.backgroundColor = '#75a7e6';
+          } else {
+            visitedAnimation(tiles[neighbor], '#75a7e6');
+          }
+          visitedTiles.push(tiles[neighbor]);
+        } 
+        prev[neighbor] = node;
+        if (endTile == neighbor) {
+          endNode = neighbor;
+          break outerLoop;
+        }
+      }
+    }
 
     
+  }
+
+  // Color the path from end to start
+  let node = endNode;
+  while (node != startTile && !resetOn) {
+    if (node !== endNode) {
+      if (delayTime == 0) {
+        tiles[node].element.style.backgroundColor = '#f5ff5e';
+      } else {
+        visitedAnimation(tiles[node], '#f5ff5e');
+        await delay(delayTime); 
+      }
+    }
+    node = prev[node];
   }
 
   // Color the path from end to start
@@ -574,14 +649,135 @@ async function dijTime(delayTime) {
 // AStar
 ///////////////////////
 
-const aStar = () => {
-  pathFindingDone = false;
-  currentAlg = 3;
-  astarTime(30)
+const getRow = (num) => {
+  return Math.floor(num / gridSizeX);
 }
 
-async function astarTime(delayTime) {
-  
+const getCol = (num) => {
+  return num % gridSizeX;
+}
+
+const calculateFCost = (tile, type) => {
+  let gCost, hCost = -1;
+  let endR = getRow(endTile);
+  let endC = getCol(endTile);
+  let startR = getRow(startTile);
+  let startC = getCol(startTile);
+
+  if (type == 'euclidean') {
+    gCost = Math.sqrt(Math.pow(getCol(tile) - startC, 2) + Math.pow(getRow(tile) - startR, 2));
+    hCost = Math.sqrt(Math.pow(getCol(tile) - endC, 2) + Math.pow(getRow(tile) - endR, 2));
+  } else if (type == 'manhattan') {
+    gCost = Math.abs(getCol(tile) - startC) + Math.abs(getRow(tile) - startR);
+    hCost = Math.abs(getCol(tile) - endC) + Math.abs(getRow(tile) - endR);
+  } else {
+    gCost = Math.abs(getCol(tile) - startC) + Math.abs(getRow(tile) - startR);
+    hCost = Math.abs(getCol(tile) - endC) + Math.abs(getRow(tile) - endR);
+  }
+
+  return gCost + hCost;
+}
+
+const addToQueue = (queue, tile, type) => {
+  const fCost = calculateFCost(tile, type);
+  const newItem = [tile, fCost];
+
+  let i = 0;
+
+  // Iterate through the queue and find the index where the new item should be inserted based on its fCost
+  while (i < queue.length && queue[i][1] <= fCost) {
+    i++;
+  }
+
+  // Insert the new item at the correct index
+  queue.splice(i, 0, newItem);
+
+  return queue;
+}
+
+
+const aStar = (type) => {
+  pathFindingDone = false;
+  if (type === 'euclidean') {
+    currentAlg = 4;
+  } else {
+    currentAlg = 5;
+  }
+  astarTime(30, type)
+}
+
+async function astarTime(delayTime, type) {
+  selectedColor = '';
+  if (!checkToStart()) {
+    return;
+  }
+
+  editMode = false;
+  resetOn = false;
+  updateTileNeighbors();
+  const graph = buildGraph();
+  const visited = new Set([startTile]);
+  let prioQueue = [[startTile, calculateFCost(startTile, type)]];
+  const prev = {};
+  let endNode = null;
+  resetVisitedTiles();
+
+  outerLoop : while (prioQueue.length > 0) {
+    if (resetOn) {
+      break;
+    }
+    if (delayTime > 0) {
+      await delay(delayTime);
+    } 
+
+    const [node, fCost] = prioQueue.shift();
+
+    if (node == endTile) {
+      endNode = node;
+      break;
+    }
+    
+    for (let neighbor of graph[node]) {
+      if (!visited.has(neighbor)) {
+        visited.add(neighbor);
+        prioQueue = addToQueue(prioQueue, neighbor, type);
+        
+        if (neighbor !== startTile && neighbor !== endTile) {
+          if (delayTime == 0) {
+            tiles[neighbor].element.style.backgroundColor = '#75a7e6';
+          } else {
+            visitedAnimation(tiles[neighbor], '#75a7e6');
+          }
+          visitedTiles.push(tiles[neighbor]);
+        } 
+        prev[neighbor] = node;
+        if (endTile == neighbor) {
+          endNode = neighbor;
+          break outerLoop;
+        }
+      }
+    }
+
+    
+  }
+
+  // Color the path from end to start
+  let node = endNode;
+  while (node != startTile && !resetOn) {
+    if (node !== endNode) {
+      if (delayTime == 0) {
+        tiles[node].element.style.backgroundColor = '#f5ff5e';
+      } else {
+        visitedAnimation(tiles[node], '#f5ff5e');
+        await delay(delayTime); 
+      }
+    }
+    node = prev[node];
+  }
+
+  // Color the path from end to start
+
+  pathFindingDone = true;
 }
 
 
@@ -595,13 +791,113 @@ async function astarTime(delayTime) {
 // Greedy BFS
 ///////////////////////
 
-const greedyBFS = () => {
-  pathFindingDone = false;
-  currentAlg = 4;
-  greedyTime(30)
+const addToQueueGreed = (queue, tile, type) => {
+  let endR = getRow(endTile);
+  let endC = getCol(endTile);
+  let dis = -1;
+
+  if (type === 'euclidean') {
+    dis = Math.sqrt(Math.pow(getCol(tile) - endC, 2) + Math.pow(getRow(tile) - endR, 2));
+  } else if (type === 'manhattan') {
+    dis = Math.abs(getCol(tile) - endC) + Math.abs(getRow(tile) - endR);
+  }
+
+  const newItem = [tile, dis];
+
+  let i = 0;
+
+  // Iterate through the queue and find the index where the new item should be inserted based on its fCost
+  while (i < queue.length && queue[i][1] <= dis) {
+    i++;
+  }
+
+  // Insert the new item at the correct index
+  queue.splice(i, 0, newItem);
+
+  return queue;
 }
 
-async function greedyTime(delayTime) {
-  console.log("Greedy Best-First Search");
+const greedyBFS = (type) => {
+  pathFindingDone = false;
+  if (type === 'euclidean') {
+    currentAlg = 6;
+  } else {
+    currentAlg = 7;
+  }
+  greedyTime(30, type)
+}
+
+async function greedyTime(delayTime, type) {
+  selectedColor = '';
+  if (!checkToStart()) {
+    return;
+  }
+
+  editMode = false;
+  resetOn = false;
+  updateTileNeighbors();
+  const graph = buildGraph();
+  const visited = new Set([startTile]);
+  let prioQueue = [[startTile, 0]];
+  const prev = {};
+  let endNode = null;
+  resetVisitedTiles();
+
+  outerLoop : while (prioQueue.length > 0) {
+    if (resetOn) {
+      break;
+    }
+    if (delayTime > 0) {
+      await delay(delayTime);
+    } 
+
+    const [node, distance] = prioQueue.shift();
+
+    if (node == endTile) {
+      endNode = node;
+      break;
+    }
+    
+    for (let neighbor of graph[node]) {
+      if (!visited.has(neighbor)) {
+        visited.add(neighbor);
+        prioQueue = addToQueueGreed(prioQueue, neighbor, type);
+        
+        if (neighbor !== startTile && neighbor !== endTile) {
+          if (delayTime == 0) {
+            tiles[neighbor].element.style.backgroundColor = '#75a7e6';
+          } else {
+            visitedAnimation(tiles[neighbor], '#75a7e6');
+          }
+          visitedTiles.push(tiles[neighbor]);
+        } 
+        prev[neighbor] = node;
+        if (endTile == neighbor) {
+          endNode = neighbor;
+          break outerLoop;
+        }
+      }
+    }
+
+    
+  }
+
+  // Color the path from end to start
+  let node = endNode;
+  while (node != startTile && !resetOn) {
+    if (node !== endNode) {
+      if (delayTime == 0) {
+        tiles[node].element.style.backgroundColor = '#f5ff5e';
+      } else {
+        visitedAnimation(tiles[node], '#f5ff5e');
+        await delay(delayTime); 
+      }
+    }
+    node = prev[node];
+  }
+
+  // Color the path from end to start
+
+  pathFindingDone = true;
 }
 
